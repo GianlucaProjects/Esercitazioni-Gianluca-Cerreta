16c62251f7ae9f298c73be37a4a7fa13088fbed5
Gianluca Cerreta <gianlucacerreta@tv569b930ea014.station>
2024-06-19 16:36:41 +0200

    esercizio 5 e 6 di JS

---
+++ JS/es5/assets/js/script.js
@@ -0,0 +1,14 @@
+let a = 10;
+let b = 12;
+
+if(a > b) {
+    console.log("Il primo numero, ovvero " + a + " è maggiore del secondo, ovvero " + b);
+}
+else if(a < b) {
+    console.log("Il primo numero, ovvero " + a + " è minore del secondo, ovvero " + b);
+}
+else {
+    console.log("Il primo numero, ovvero " + a + " è uguale al secondo, ovvero " + b);
+}
+
+5 === 5 ? console.log("Si il numero è lo stesso") : console.log("No, sono due numeri diversi!");
